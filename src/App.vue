0d7c923fa2149635512ed5920869b8cb9a20ecfb
khidd <kennedynyambura5981@gmail.com>
2023-06-08 20:42:14 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>{{ title }}</h1>
+<div v-if="showModal">
+  <Modal theme="" @close="toggleModal">
+    <template v-slot:links>
+      <a href="#">sign up now!</a>
+      <a href="#">more information</a>
+    </template>
+    <h1>Kennedy Give away</h1>
+    <p>Get yourself signals at half price</p>
+</Modal>
+</div>
+<div v-if="showModalTwo">
+  <Modal @close="toggleModalTwo">
+    <h1>Sign up for classes</h1>
+    <p>For newsupdates and classes</p>
+</Modal>
+</div>
+<p>wecome here</p>
+<button @click.alt="toggleModal">Open Modal (alt)</button>
+<button @click="toggleModalTwo">Open Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue' 
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  components:{Modal},
+  data(){
+    return{
+      title:'My First Vue App',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import'./components/global.css'
 
 createApp(App).mount('#app')
